4e6e927fcdeb3d8ef7a91e764bf5a171f144f824
Henrique Eugenio Dos Santos <henrique.esantos153@gmail.com>
2025-12-02 13:26:20 -0300

    Exercício 4
    Estrutura Pessoa e Funções Básicas.

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
-#include "operacoes.h"
+#include "pessoa.h"
 
-int main(void) 
-{
-    printf("a soma de 2 e 3 eh: %d", soma(2,3));
+int main() {
+    char nome[50];
+    int idade;
+    
+    // Ler o nome
+    printf("Digite o nome: ");
+    fgets(nome, sizeof(nome), stdin);
+    
+    // Ler a idade
+    printf("Digite a idade: ");
+    scanf("%d", &idade);
+
+    // Criar a pessoa usando a função
+    Pessoa p = criarPessoa(nome, idade);
+
+    // Mostrar os dados da pessoa
+    mostrarPessoa(p);
+
     return 0;
 }
